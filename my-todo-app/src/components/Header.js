dc288bd842317a14c3f3575a22916c184bdd4bf2
unknown <protyusha.ahmed@gmail.com>
2022-12-30 14:13:24 +0530

    css fixes

--- my-todo-app/src/components/Header.js
+++ my-todo-app/src/components/Header.js
@@ -13,7 +13,7 @@ export const Header = () => {
 
     return(
         <div style={headerStyle}>
-            <h1>Todo Items</h1>
+            <h1>Todo API call and displaying data </h1>
         </div>
     )
 }
